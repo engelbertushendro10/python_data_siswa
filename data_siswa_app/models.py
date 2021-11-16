323df97e7723a372ff8a5dd8a6ee43e1534ce2e2
engelbertushendro10 <engelbertushendro@gmail.com>
2021-11-15 19:28:29 -0800

    super user

--- data_siswa_app/models.py
+++ data_siswa_app/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.db.models.fields.related import OneToOneField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 class CustemUser(AbstractUser):
@@ -11,9 +13,6 @@ class CustemUser(AbstractUser):
 class AdminHOD(models.Model):
     id=models.AutoField(primary_key=True)
     admin=models.OneToOneField(CustemUser,on_delete=models.CASCADE)
-    name=models.CharField(max_length=255)
-    email=models.CharField(max_length=255)
-    password=models.CharField(max_length=255)
     created_at=models.DateTimeField(auto_now_add=True)
     update_ad=models.DateTimeField(auto_now_add=True)
     objects=models.Manager()
@@ -21,9 +20,6 @@ class AdminHOD(models.Model):
 class Staff(models.Model):
     id=models.AutoField(primary_key=True)
     admin=models.OneToOneField(CustemUser,on_delete=models.CASCADE)
-    name=models.CharField(max_length=255)
-    email=models.CharField(max_length=255)
-    password=models.CharField(max_length=255)
     address=models.TextField()
     created_at=models.DateTimeField(auto_now_add=True)
     update_at=models.DateTimeField(auto_now_add=True)
@@ -48,9 +44,6 @@ class Subjects(models.Model):
 class Students(models.Model):
     id=models.AutoField(primary_key=True)
     admin=models.OneToOneField(CustemUser,on_delete=models.CASCADE)
-    name=models.CharField(max_length=255)
-    email=models.CharField(max_length=255)
-    password=models.CharField(max_length=255)
     gender=models.CharField(max_length=255)
     profile_pic=models.CharField(max_length=255)
     address=models.TextField()
@@ -132,3 +125,25 @@ class NotificationStaff(models.Model):
     create_at=models.DateTimeField(auto_now_add=True)
     update_at=models.DateTimeField(auto_now_add=True)
     objects=models.Manager()
+
+@receiver(post_save, sender=CustemUser)
+def create_user_profile(sender, instance, created,**kwargs):
+    if created:
+        if instance.user_type==1:
+            AdminHOD.objects.create(admin=instance)
+        if instance.user_type==2:
+            Staff.objects.create(admin=instance)
+        if instance.user_type==3:
+            Students.objects.create(admin=instance)
+
+@receiver(post_save, sender=CustemUser)
+def save_user_profile(sender, instace, **kwargs):
+    if instace.user_type==1:
+        instace.AdminHOD.save()
+    if instace.user_type==2:
+        instace.Staff.save()
+    if instace.user_type==3:
+        instace.Students.save()
+
+    
+
